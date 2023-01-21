a1f21890cb8acc7247da282dcde5019e43a2662a
waseem1104 <waseem11@hotmail.fr>
2023-01-21 15:10:41 +0100

    Change view session cinema and entity

--- api/src/Entity/MovieScreening.php
+++ api/src/Entity/MovieScreening.php
@@ -23,11 +23,11 @@ class MovieScreening
     #[ORM\Column]
     private ?float $price = null;
 
-    #[ORM\Column(type: Types::ARRAY)]
-    private array $disposition_room = [];
-
     #[ORM\ManyToOne(inversedBy: 'movieScreenings')]
     private ?User $creator = null;
+
+    #[ORM\Column]
+    private ?int $room = null;
 
     public function getId(): ?int
     {
@@ -58,18 +58,6 @@ class MovieScreening
         return $this;
     }
 
-    public function getDispositionRoom(): array
-    {
-        return $this->disposition_room;
-    }
-
-    public function setDispositionRoom(array $disposition_room): self
-    {
-        $this->disposition_room = $disposition_room;
-
-        return $this;
-    }
-
     public function getCreator(): ?User
     {
         return $this->creator;
@@ -78,6 +66,18 @@ class MovieScreening
     public function setCreator(?User $creator): self
     {
         $this->creator = $creator;
+
+        return $this;
+    }
+
+    public function getRoom(): ?int
+    {
+        return $this->room;
+    }
+
+    public function setRoom(int $room): self
+    {
+        $this->room = $room;
 
         return $this;
     }
