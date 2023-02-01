270cc1d303951f7b14ea38ae5c180bee5b901f78
waseem1104 <waseem11@hotmail.fr>
2023-02-01 21:35:55 +0100

    Session View with searchBar cinema - DONE

--- client/src/components/Session.vue
+++ client/src/components/Session.vue
@@ -1,86 +1,169 @@
 <template>
-    <div>
-        <div class="row mt-4" v-for="(movie,i) in movies" :key="i">
-                <div class="card m-2 shadow-sm card-global">
-                    <div class="card-body m-2">
-                        <div class="d-flex">
-                            <div><img class="movie-picture" :src="getImageFromSrc(movie.details.backdrop_path)"></div> 
-                            <div>
-                                <div class="mx-4">
-                                    <h4 class="card-title">{{movie.details.title}}</h4>
-                                    <!-- <h5 class="card-subtitle mb-2 text-muted">Le Grand Rex</h5> -->
-                                    <p class="card-subtitle mb-2 text-muted">{{movie.details.overview}}</p>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="session mt-4">
-                            <a :href="'/booking?id=' + session.id" class="card-link" v-for="(session,i) in movie.sessions" :key="i">
-                                <div class="card card-datetime">
-                                    <h6>{{session.sessionDatetime.split("T")[0]}}</h6>
-                                    <h6>{{session.sessionDatetime.split("T")[1].substr(0,5)}}</h6>
-                                    <h6>VF</h6>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-        </div>
+  <div>
+    <div class="container mb-3 mt-3">
+      <select class="form-select" @change="handleChange($event)">
+        <option
+          v-for="(cinema, index) in cinema.value"
+          :key="index"
+          :value="cinema.id"
+        >
+          {{ cinema.name }}
+        </option>
+      </select>
     </div>
+    <div class="row mt-4" v-for="(movie, i) in movies" :key="i">
+      <div class="card m-2 shadow-sm card-global">
+        <div class="card-body m-2">
+          <div class="d-flex">
+            <div>
+              <img
+                class="movie-picture"
+                :src="getImageFromSrc(movie.details.backdrop_path)"
+              />
+            </div>
+            <div>
+              <div class="mx-4">
+                <h4 class="card-title">{{ movie.details.title }}</h4>
+                <p class="card-subtitle mb-2 text-muted">
+                  {{ movie.details.overview }}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="session mt-4">
+            <a
+              :href="'/booking?id=' + session.id"
+              class="card-link"
+              v-for="(session, i) in movie.sessions"
+              :key="i"
+            >
+              <div class="card card-datetime">
+                <h6>{{ session.session_datetime.split("T")[0] }}</h6>
+                <h6>
+                  {{ session.session_datetime.split("T")[1].substr(0, 5) }}
+                </h6>
+                <h6>VF</h6>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
-import { reactive,ref } from 'vue';
-import {
-    getImageFromSrc,
-} from "../utils/tmdbCalls";
+import { onMounted, watch } from "vue";
+import { reactive, ref } from "vue";
+import { getImageFromSrc } from "../utils/tmdbCalls";
 
-const result = reactive({value : []})
-const movie = reactive({value : []})
-const movies = ref([])
+const result = reactive({ value: [] });
+const movie = reactive({ value: [] });
+const movies = ref([]);
+const cinema = reactive({ value: [] });
+const search = ref("");
 
-onMounted( async () =>{
-    await fetchSessions();
+const fetchCinema = async () => {
+  return fetch("https://localhost/users")
+    .then((response) => response.json())
+    .then(
+      (data) =>
+        (cinema.value = data["hydra:member"].filter(
+          (x) =>
+            x.enabled == true &&
+            x.roles.includes("ROLE_CINEMA") &&
+            x.status == 1
+        ))
+    );
+};
 
-    // Date supérieur > NOW
-    result.value = result.value.filter(i => new Date(i.sessionDatetime) > new Date());
-    // Remplacer
-    result.value = result.value.filter(i => i.creator == "/users/1");
+watch(search, async (newSearch) => {
+  await fetchSessions();
 
-    // Tableau de film unique
-    let unique =  result.value.filter((item, index, self) => self.findIndex(t => t.movieId === item.movieId) === index);
+  result.value = result.value.filter(
+    (i) => new Date(i.session_datetime) > new Date()
+  );
+  result.value = result.value.filter((i) => i.creator.id == newSearch);
+  let unique = result.value.filter(
+    (item, index, self) =>
+      self.findIndex((t) => t.movie_id === item.movie_id) === index
+  );
 
-    for (let i = 0; i < unique.length; i++){
-        await fetchMovie(unique[i].movieId);
-        let sessions = result.value.filter( j => j.movieId == unique[i].movieId);
-        movies.value.push( {
-            "details" : movie.value,
-            "sessions" : sessions
-        });
-    }
-   
-})
+  movies.value = [];
+
+  for (let i = 0; i < unique.length; i++) {
+    await fetchMovie(unique[i].movie_id);
+
+    let sessions = result.value.filter((j) => j.movie_id == unique[i].movie_id);
+
+    movies.value.push({
+      details: movie.value,
+      sessions: sessions,
+    });
+  }
+});
+
+onMounted(async () => {
+  await fetchCinema();
+  search.value = cinema.value[0].id;
+});
+
+const handleChange = (e) => {
+  search.value = e.target.value;
+};
 
 const fetchMovie = async (id) => {
-    
-    return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${
       import.meta.env.VITE_TMDB_API_KEY
-    }&language=fr`)
+    }&language=fr`
+  )
     .then((response) => response.json())
-    .then((data) => {movie.value = data});
-}
+    .then((data) => {
+      movie.value = data;
+    });
+};
 
 const fetchSessions = async () => {
-    return fetch('https://localhost/movie_screenings')
+  return fetch("https://localhost/movie_screenings")
     .then((response) => response.json())
-    .then((data) => (result.value = data['hydra:member']));
-}
+    .then((data) => (result.value = data["hydra:member"]));
+};
 </script>
 
-
 <style scoped>
+.card-global {
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+  background-color: #181818;
+  color: var(--color-white);
+}
+.movie-picture {
+  width: 200px;
+  object-fit: cover;
+  border-radius: 5px;
+}
+
+.card-datetime {
+  display: flex;
+  height: 100px;
+  width: 150px;
+  padding: 12px;
+  border-radius: 5px;
+  background-color: #2f2f2f;
+  color: var(--color-white);
+  text-decoration: none;
+  font-size: 12px;
+}
+.session {
+  display: flex;
+  overflow-x: scroll;
+}
+.card-link {
+  text-decoration: none;
+}
 
 .input-search {
   border-radius: 15px;
@@ -88,36 +171,4 @@ const fetchSessions = async () => {
   padding-left: 2em;
   background-color: #ffffff30;
 }
-.card-global{
-    width: 100%;
-    height: auto;
-    border-radius: 10px;
-    background-color: #181818;
-    color: var(--color-white);
-}
-.movie-picture{
-    width: 200px;
-    object-fit: cover;
-    border-radius: 5px;
-}
-
-.card-datetime{
-    display: flex;
-    height: 100px;
-    width: 150px;
-    padding: 12px;
-    border-radius: 5px;
-    background-color: #2f2f2f;
-    color: var(--color-white);
-    text-decoration: none;
-    font-size: 12px;
-}
-.session{
-    display: flex;
-    overflow-x: scroll;
-}
-.card-link{
-    text-decoration: none;
-}
-
 </style>
--- client/src/views/cinema/SessionView.vue
+++ client/src/views/cinema/SessionView.vue
@@ -1,35 +1,15 @@
 <script setup>
-import Session from '../../components/Session.vue';
-import SessionsBanner from '../../components/SessionsBanner.vue';
-import {onUpdated, reactive, ref} from 'vue'
-
-
-
-
+import Session from "../../components/Session.vue";
+import HeaderBanner from "../../components/HeaderBanner.vue";
 </script>
 
 <template>
   <main>
+    <HeaderBanner title="Séances" img="./src/assets/cinema.jpeg" />
     <div class="container mt-2">
-      <div class="d-flex title mt-4 mb-3">
-          <h1 class="flex-shrink-0">Séances</h1>
-          <p class="flex-grow-1 ms-2 m-auto fs-6">- Toutes nos séances</p>
-      </div>
-      <hr>
-      <SessionsBanner/>
-      <Session/>
+      <Session />
     </div>
   </main>
 </template>
 
-
-<style scoped>
-
-.title {
-  color: var(--color-white);
-}
-
-.title h1 {
-  font-size: 30px;
-}
-</style>
+<style scoped></style>
