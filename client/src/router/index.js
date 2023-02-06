e357280bcb4b7a9fae7b6d927f15132c54712f70
jehanebnj <jehanebnj@gmail.com>
2023-02-07 00:31:45 +0100

    Register page for cinema

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/auth/Login.vue";
 import RegisterView from "../views/auth/Register.vue";
+import RegisterCinemaView from "../views/auth/RegisterCinema.vue";
 import ForgetPasswordView from "../views/auth/ForgetPassword.vue";
 import UserAccountView from "../views/auth/UserAccount.vue";
 import EnableAccountView from "../views/auth/EnableAccount.vue";
@@ -30,6 +31,11 @@ const router = createRouter({
       path: "/register",
       name: "register",
       component: RegisterView,
+    },
+    {
+      path: "/register-cinema",
+      name: "register-cinema",
+      component: RegisterCinemaView,
     },
     {
       path: "/forget-password",
---
+++ client/src/views/auth/RegisterCinema.vue
@@ -0,0 +1,174 @@
+<script setup>
+import { reactive, ref } from "vue";
+
+const formInputs = reactive({
+  name: "",
+  address: "",
+  email: "",
+  password: "",
+  passwordConfirm: "",
+});
+
+const errorMessage = ref(null);
+
+const handleSubmitForm = async (e) => {
+  e.preventDefault();
+  const requestData = {
+    name: formInputs.name,
+    adress: formInputs.address,
+    email: formInputs.email,
+    password: formInputs.password,
+    isCinema: true,
+    status: '0'
+  };
+
+  fetch(`${import.meta.env.VITE_API_SERVER_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+</script>
+
+<template>
+  <div class="register-vue_background">
+    <div class="card card-register shadow-sm">
+      <div class="text-center">
+        <h3 class="pt-3">S'inscrire</h3>
+      </div>
+      <div class="card-body">
+        <form
+          class="d-flex flex-column justify-content-center"
+          @submit="handleSubmitForm"
+        >
+          <div class="form-group">
+            <input
+              type="name"
+              placeholder="Nom"
+              name="email"
+              id="inputMail"
+              class="form-control"
+              v-model="formInputs.name"
+              autocomplete="email"
+              required
+              autofocus
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              placeholder="Adresse"
+              name="address"
+              id="inputAddress"
+              class="form-control"
+              v-model="formInputs.address"
+              autocomplete="address"
+              required
+              autofocus
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="email"
+              placeholder="E-mail"
+              name="email"
+              id="inputMail"
+              class="form-control"
+              v-model="formInputs.email"
+              autocomplete="email"
+              required
+              autofocus
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              name="password"
+              id="inputPwd"
+              class="form-control"
+              v-model="formInputs.password"
+              autocomplete="current-password"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="password"
+              placeholder="Confirmation de mot de passe"
+              name="password"
+              id="inputPwd"
+              class="form-control"
+              v-model="formInputs.passwordConfirm"
+              autocomplete="current-password"
+              required
+            />
+          </div>
+          <span>{{ errorMessage }}</span>
+          <div class="d-flex justify-content-center">
+            <button class="btn mt-4 btn-cinemax" type="submit">
+              <span>S'inscrire</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card-register {
+  width: 450px;
+  margin: 150px auto auto;
+  margin-top: 150px;
+  padding: 20px;
+  background-color: var(--color-black);
+  color: var(--color-white);
+  border-radius: 10px;
+}
+.btn-cinemax {
+  background-color: var(--color-red);
+  color: var(--color-white);
+  text-align: center;
+  width: 100%;
+}
+.btn-cinemax:hover {
+  background-color: var(--color-darkred);
+  color: var(--color-white);
+}
+#inputMail {
+  background-color: #ffffff30;
+  color: var(--color-white);
+  border: none;
+}
+#inputAddress {
+  background-color: #ffffff30;
+  color: var(--color-white);
+  border: none;
+}
+#inputPwd {
+  background-color: #ffffff30;
+  color: var(--color-white);
+  border: none;
+}
+#forgetPassword {
+  font-size: 12px;
+  margin-top: 20px;
+  color: #ffffff95;
+}
+.register-vue_background {
+  display: flex;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.604), rgba(0, 0, 0, 0.649)),
+    url("../../assets/background.jpeg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 96vh;
+}
+</style>
